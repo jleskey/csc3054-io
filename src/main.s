4cd20dfc6151d71ef2bcbe77036cf60c0289220f
Joseph Leskey <joseph@josephleskey.com>
2024-11-05 13:11:32 -0500

    Implement print, measureString functions

--- src/main.s
+++ src/main.s
@@ -16,8 +16,39 @@ end:        .asciz  ".\n"
 main:
     j exit
 
-input:
+# int inputWord : Prompt user for an integer
+# a0 <string *prompt> : the prompt address
+inputWord:
     nop
+
+# int print : Print the given string
+# a0 <string *value> : the string address
+print:
+    mv      a1, a0                          # a1: string address
+
+    jal     measureString
+    mv      a2, a0                          # a2: length of string
+
+    li      a7, SYS_WRITE                   # a7: syscall
+    li      a0, STDOUT                      # a0: file descriptor
+
+    ecall
+
+    ret
+
+# int measureString : Get length of given string
+# a0 <string *value> : the string address
+measureString:
+    li      t0, 0                           # t0: counter
+    measureStringLoop:
+        lb      t1, 0(a0)                   # t1: character in string
+        beqz    t1, measureStringExit
+        addi    t0, t0, 1
+        addi    a0, a0, 1
+        j       measureStringLoop
+    measureStringExit:
+        mv      a0, t0                      # a0 (return): length of string
+        ret
 
 exit:
     li      a0, 0
